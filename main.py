400a77c00709cd8b8da7d754d0c9cfe913add6ac
Lisa Vandi <lisa.vandi2@studio.unibo.it>
2026-01-31 15:53:00 +0100

    main completed

--- main.py
+++ main.py
@@ -30,43 +30,42 @@ def preprocess_text(df, remove_duplicates=False):
         if 'label' in df.columns:
             df['count_per_label'] = df.groupby(['title', 'article', 'label'])['title'].transform('count')
             df['max_count_for_text'] = df.groupby(['title', 'article'])['count_per_label'].transform('max')
+
             df_majority = df[df['count_per_label'] == df['max_count_for_text']].copy()
             
-            # 4. Step Tie-Breaker: Gestione dei Pareggi (es. 1 Sport vs 1 Business)
             inconsistent_mask = df_majority.duplicated(subset=['title', 'article'], keep=False)
             ties = df_majority[inconsistent_mask].groupby(['title', 'article'])['label'].nunique()
             ties_indices = ties[ties > 1].index
-            
+
             df_clean = df_majority.set_index(['title', 'article']).drop(index=ties_indices, errors='ignore').reset_index()
+            
             df = df_clean.drop_duplicates(subset=['title', 'article'], keep='first').copy()
             
             drop_cols = ['count_per_label', 'max_count_for_text']
             df = df.drop(columns=[c for c in drop_cols if c in df.columns])
             
         else:
-            df = df.drop_duplicates(subset=['title', 'article'], keep='first').copy()  
+            df = df.drop_duplicates(subset=['title', 'article'], keep='first').copy()
     else:
         df = df.copy()
         
-    # Feature weighting    
+    # Feature weighting: boost importance of Source and Title     
     source_feature = (df['source'].fillna('') + " ") * 3
-    title_weighted  = (df['title'].fillna('') + " ") * 2
+    title_weighted  = (df['title'].fillna('') + " ") * 3
     df['text_combined'] = source_feature + title_weighted + " " + df['article'].fillna('')
     
-    # Stemming
     stemmer = SnowballStemmer("english")
     
     def clean(text):
         text = str(text).lower()
         text = html.unescape(text)
-        text = re.sub(r'<[^>]+>', ' ', text)
-        text = re.sub(r'[^a-z0-9\s$€%£]', ' ', text) # useful symbols for class=business (€,%,$)                  
+        text = re.sub(r'<[^>]+>', ' ', text) 
+        text = re.sub(r'[^a-z0-9\s$€%£]', ' ', text)       
         text = re.sub(r'\s+', ' ', text).strip()
-        return " ".join([stemmer.stem(word) for word in text.split()]) # stemming
+        return " ".join([stemmer.stem(word) for word in text.split()])
 
     df['clean_text'] = df['text_combined'].apply(clean)
     return df
-
 
 def extract_time_features(df):
     df = df.copy()
@@ -75,7 +74,6 @@ def extract_time_features(df):
     df['hour'] = df['timestamp_dt'].dt.hour.fillna(-1)
     df['day_of_week'] = df['timestamp_dt'].dt.dayofweek.fillna(-1)
     df = df.drop(columns=['timestamp_dt'])
-    
     return df
 
 def main():    
@@ -86,39 +84,52 @@ def main():
     dev_df = pd.read_csv(TRAIN_FILE)
     eval_df = pd.read_csv(EVAL_FILE)
     
-    dev_df = extract_time_features(dev_df, remove_duplicates=True)
-    eval_df = extract_time_features(eval_df, remove_duplicates=False)
+    eval_ids = eval_df['Id']
     
-    feature_cols = ['clean_text', 'page_rank', 'hour', 'has_date']
+    dev_df['article'] = dev_df['article'].replace('\\N', '')
+    eval_df['article'] = eval_df['article'].replace('\\N', '')
+
+    dev_df = extract_time_features(dev_df)
+    eval_df = extract_time_features(eval_df) 
+
+    dev_df = preprocess_text(dev_df, remove_duplicates=True)
+    eval_df = preprocess_text(eval_df, remove_duplicates=False) 
+
+    dev_df = dev_df[dev_df['clean_text'].str.len() > 20].copy()
     
-    X = dev_df[feature_cols]
+    feature_cols = ['clean_text', 'source', 'page_rank', 'hour', 'day_of_week', 'has_date']
+    
+    X = dev_df[feature_cols] 
     y = dev_df['label']
     
-    text_features = 'clean_text'
-    num_features = ['page_rank', 'hour', 'has_date']
-    
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('txt', TfidfVectorizer(min_df=3, max_df=0.9, ngram_range=(1,2)), text_features),
-            ('num', MinMaxScaler(), num_features)
-        ],
-        remainder='drop' 
-    )
-    
-    # Pipeline Completa
-    model_pipeline = Pipeline([
-        ('preprocessor', preprocessor),
-        ('clf', LinearSVC(class_weight='balanced', random_state=SEED))
+    final_text_pipeline = Pipeline([
+        ('tfidf', TfidfVectorizer(
+            stop_words='english', 
+            min_df=3, 
+            sublinear_tf=True,
+            ngram_range=(1,2)
+        )),
+        ('selector', SelectKBest(chi2, k=50000))
+    ])
+
+    final_pipeline = Pipeline([
+        ('preprocessor', ColumnTransformer(
+            transformers=[
+                ('tfidf', final_text_pipeline, 'clean_text'),
+                ('ohe', OneHotEncoder(handle_unknown='ignore', min_frequency=5), ['source','day_of_week']), 
+                ('num', MinMaxScaler(), ['page_rank', 'hour', 'has_date'])
+            ]
+        )),
+        ('clf', LinearSVC(
+            class_weight='balanced', 
+            random_state=42, 
+            dual='auto',
+            C=0.2
+        ))
     ])
     
-    # 4. Training
-    model_pipeline.fit(X_train, y_train)
-    
-    # 5. Valutazione
-    print("Valutazione...")
-    y_pred = model_pipeline.predict(X_val)
-    print(classification_report(y_val, y_pred))
-    print(f"F1 Macro: {f1_score(y_val, y_pred, average='macro'):.4f}")
+    final_pipeline.fit(X, y)
+    y_pred_eval = final_pipeline.predict(eval_df)
 
     pd.DataFrame({
         'Id': eval_ids,
